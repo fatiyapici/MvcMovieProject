9989ec1993ec7a034cc4e98a15631d96bf974510
Fatih Yapıcı <fa.yapici@gmail.com>
2023-08-15 16:39:42 +0300

    add new field with db update

--- Migrations/20230815133834_InitialCreate.Designer.cs
+++ Migrations/20230815133834_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using MvcMovie.Data;
 namespace MvcMovie.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    [Migration("20230807104421_InitialCreate")]
+    [Migration("20230815133834_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
@@ -30,6 +30,9 @@ namespace MvcMovie.Migrations
                         .HasColumnType("TEXT");
 
                     b.Property<decimal>("Price")
+                        .HasColumnType("decimal(18, 2)");
+
+                    b.Property<string>("Rating")
                         .HasColumnType("TEXT");
 
                     b.Property<DateTime>("ReleaseDate")
